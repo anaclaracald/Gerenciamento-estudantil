daf12a0ba97f7eac65cf3f41d6d2ebab3d8bcd00
Alice Ferreira <alice.fandrade@a.ucb.br>
2024-11-23 15:30:03 -0300

    adiciona janelas

--- src/main/java/poo/ucb/Gerenciamento_Estudantil/view/ConsultarEstudante.java
+++ src/main/java/poo/ucb/Gerenciamento_Estudantil/view/ConsultarEstudante.java
@@ -1,14 +1,9 @@
 package poo.ucb.Gerenciamento_Estudantil.view;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import poo.ucb.Gerenciamento_Estudantil.model.services.EstudanteService;
-
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-@Component
 public class ConsultarEstudante extends JFrame {
     private JTextField textFieldNome;
     private JTextField textFieldMatricula;
@@ -16,9 +11,6 @@ public class ConsultarEstudante extends JFrame {
     private JButton buttonEnviarMatricula;
     private JButton buttonVoltar;
     private JPanel janelaConsEstudante;
-
-    @Autowired
-    private EstudanteService estudanteService;
 
     public ConsultarEstudante() {
     // Primeiras configurações
@@ -38,22 +30,17 @@ public class ConsultarEstudante extends JFrame {
         buttonEnviarNome.addActionListener(new ActionListener() { // Botão Enviar (nome)
             @Override
             public void actionPerformed(ActionEvent e) {
-
                 String nome = textFieldNome.getText();
+                // TODO enviar nome para pesquisa na DB
 
-<<<<<<< HEAD
-                try{
-                    // (estudante encontrado) {apresentar tela de editar ou excluir}
-                    estudanteService.buscarPorNome(nome);
-=======
                 // if (estudante encontrado) {apresentar tela de editar ou excluir}
                 int i = 0; // Valor arbitrário para testes
                 if (i == 0) {
->>>>>>> 36448243fd9f79723be6132701fa05bdb1d9eea0
                     EditarEstudante janelaEditEstudante = new EditarEstudante();
                     janelaEditEstudante.setVisible(true);
-                }catch ( Exception ex){
-                    JOptionPane.showMessageDialog(ConsultarEstudante.this, "Esse estudante está vinculado ao sistema: " + ex.getMessage());
+                }
+                else {
+                    JOptionPane.showMessageDialog(ConsultarEstudante.this, "Desculpe, esse estudante não foi encontrado");
                 }
             }
         });
@@ -61,23 +48,13 @@ public class ConsultarEstudante extends JFrame {
         buttonEnviarMatricula.addActionListener(new ActionListener() { // Botão Enviar (matrícula)
             @Override
             public void actionPerformed(ActionEvent e) {
-<<<<<<< HEAD
-                try { // Pegar input de matrícula
-                    long matricula = Long.parseLong(textFieldMatricula.getText());
-                    estudanteService.buscarPorMatricula(matricula);
-                    EditarEstudante janelaEditEstudante = new EditarEstudante();
-                    janelaEditEstudante.setVisible(true);
-=======
                 // Pegar input de matrícula
                 try {
                     int matricula = Integer.parseInt(textFieldMatricula.getText());
->>>>>>> 36448243fd9f79723be6132701fa05bdb1d9eea0
                 }
                 catch (NumberFormatException exception) {
                     JOptionPane.showMessageDialog(ConsultarEstudante.this, "Insira um valor numérico.");
                 }
-<<<<<<< HEAD
-=======
 
                 // TODO enviar matrícula para pesquisa na DB
 
@@ -89,7 +66,6 @@ public class ConsultarEstudante extends JFrame {
                     EditarEstudante janelaEditEstudante = new EditarEstudante();
                     janelaEditEstudante.setVisible(true);
                 }
->>>>>>> 36448243fd9f79723be6132701fa05bdb1d9eea0
             }
         });
     }
