f97e7e02127f6802ef43dfdfa1e191904e3e9916
Alice Ferreira <alice.fandrade@a.ucb.br>
2024-11-24 18:12:33 -0300

    adiciona: CadastrarProfessor, ConsultarProfessor e EditarProfessor

--- src/main/java/poo/ucb/Gerenciamento_Estudantil/model/entities/Professor.java
+++ src/main/java/poo/ucb/Gerenciamento_Estudantil/model/entities/Professor.java
@@ -29,6 +29,10 @@ public class Professor extends Pessoa {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getEspecialidade() {
         return especialidade;
     }
